8bb412bb1684a4259ca07db2092827d9b984027c
patelaaniket <32579245+patelaaniket@users.noreply.github.com>
2021-05-28 14:35:52 -0500

    Add files via upload
    added coordinates

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from schemdraw.logic import *
 import numpy as np
 import re
 
-path = r"/home/patelaaniket/Documents/HDL-Viz-Project/LogicGate1.hdl"
+path = r"/home/patelaaniket/Documents/HDL-Viz-Project/LogicGate2.hdl"
 n = 2
 
 gmatrix, wmatrix, phdl = gd.gate_data(path, n)
@@ -33,8 +33,10 @@ class Box(elm.Element):
         super().__init__()
         x = coor[0]
         y = coor[1]
-        print(x,y)
         self.segments.append(Segment([(x, y), (x+1, y), (x+1, y+1), (x, y+1), (x, y)]))
+        self.segments.append(Segment([(x-0.3, y+0.7), (x, y+0.7)]))
+        self.segments.append(Segment([(x-0.3, y+0.3), (x, y+0.3)]))
+        self.segments.append(Segment([(x+1, y+0.5), (x+1.3, y+0.5)]))
 
 d = schemdraw.Drawing()
 
@@ -43,9 +45,9 @@ test = phdl["parts"]
 for i in range(len(gmatrix)):
     for j in range(len(gmatrix[i])):
         if (gmatrix[i][j] != -1):
+            test[gmatrix[i][j].astype(np.int64)]["coord"] = [[(j*2 - 0.3, i*3 + 0.7), (j*2 - 0.3, i*3 + 0.3)], [(j*2 + 1.3, i*3 + 0.5)]]
             fullname = test[gmatrix[i][j].astype(np.int64)]["name"]
             name = re.split('\d+', fullname)[0]
-            print(name)
             gate = Box([0, 0]).label(fullname).at((j*2, i*3 - 0.5))
             if name == "Mux":
                 gate = Mux([j*2, i*3]).label(fullname)
@@ -65,8 +67,9 @@ for i in range(len(gmatrix)):
                 gate = logic.Xnor().at((j*2, i*3))
             elif name == "Not":
                 gate = logic.Not().at((j*2, i*3))
+            else:
+                test[gmatrix[i][j].astype(np.int64)]["coord"] = [[(j*2 - 0.3, i*3 + 0.2), (j*2 - 0.3, i*3 - 0.2)], [(j*2 + 1.3, i*3)]]
             if gate != None:
                 d.add(gate)
-
 
 d.save('schematic.svg')
