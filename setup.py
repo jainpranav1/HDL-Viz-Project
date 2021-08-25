7e38d6e8249d0b700da9742470c62ec04b541ea1
Teddy Heinen <teddy@heinen.dev>
2021-08-25 15:20:15 -0500

    add osx?

--- setup.py
+++ setup.py
@@ -4,9 +4,17 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
 # "packages": ["os"] is used as example only
+
+def get_include_files():
+    if sys.platform == "linux":
+        return ("../parse/target/release/libnand2tetris_hdl_parser.so", "lib/nand2tetris_hdl_parser.cpython-39-x86_64-linux-gnu.so")
+    elif sys.platform == "darwin":
+        return ("../parse/target/release/libnand2tetris_hdl_parser.so", "lib/nand2tetris_hdl_parser.cpython-39-darwin.so")
+
+
 build_exe_options = {
     "packages": ["nand2tetris_hdl_parser"],
-    "include_files": [("../parse/target/release/libnand2tetris_hdl_parser.so", "lib/nand2tetris_hdl_parser.cpython-39-x86_64-linux-gnu.so")],
+    "include_files": [get_include_files()],
     "build_exe": "dist"
 
 }
